a8820380dcab50dadc956bdd822aa1aa114e337d
tiago13460-beep <tiago13460@gmail.com>
2026-08-29 17:37:09 -0300

    Update database connection to use PostgreSQL
    Replace MySQL connection with PostgreSQL using environment variable for database URL.

--- conexao.py
+++ conexao.py
@@ -1,12 +1,13 @@
-import mysql.connector
+import os
+import psycopg2
 
 def criar_conexao():
-
-    conexao = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='app_mulheres'
-    )
-
+    # O Render vai ler a URL do banco que vamos configurar nas variáveis de ambiente
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    
+    if not DATABASE_URL:
+        raise ValueError("A variável de ambiente DATABASE_URL não foi configurada no Render.")
+        
+    # Abre a conexão com o PostgreSQL do Render
+    conexao = psycopg2.connect(DATABASE_URL)
     return conexao
